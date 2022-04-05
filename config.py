37b4ce436fcdd3660d5b08c34e9a3612e5a69339
nickmichiels100 <102953283+nickmichiels100@users.noreply.github.com>
2022-04-05 15:54:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from dash import Dash
-import dash_bootstrap_components as dbc
+import os
+from random import randint
+import flask
 
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     # 'https://codepen.io/chriddyp/pen/bWLwgP.css'
-dashApp = Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0'}])
+dashApp = Dash(__name__,server=server, external_stylesheets=external_stylesheets, meta_tags=[{'name': 'viewport','content': 'width=device-width, initial-scale=1.0'}])
